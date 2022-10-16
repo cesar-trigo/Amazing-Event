dc40a9f0e8a56c64537595334265de027c7ca022
zincel <106347425+zincel@users.noreply.github.com>
2022-10-16 14:17:07 -0300

    eventos-de-serch

--- js/index.js
+++ js/index.js
@@ -1,28 +1,38 @@
-let contenedor = document.getElementById("container")
+let contenedorTarjet = document.getElementById("container");
+let datos = data.events;
 
-function contenido(contenido) {
-    for (let event of events) {
-        contenido.innerHTML +=
-            `
-        <div class=" colorDetexto card mb-3 p-0 rounded-5  bg-dark" style="max-width: 540px;">
-            <div class="row g-0" style="height: 100%;">
-                <div class="col-md-4 ">
-                    <img src="${event.image}" class="col-md-12 rounded-5" alt="${event.name}"
-                    style="height:100%; width: 100%;">
-                </div>
-            <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${event.name}</h5>
-                        <p class="card-text">${event.category}</p>
-                        <div class="row">
-                            <p class="card-text col-md-6"><small class="text-muted ">price: $ ${event.price}</small></p>
-                            <a href="#" class=" btn btn-dark col-md-6 rounded-5 align-self-end bg-black">Read more</a>
-                        </div>
-                    </div>
-                </div>
+
+let tarjet = datos.map(dato => (contenedorTarjet.innerHTML +=
+    `
+<div class=" colorDetexto card mb-3 p-0 rounded-5  bg-dark" style="max-width: 540px;">
+<div class="row g-0" style="height: 100%;">
+    <div class="col-md-4 ">
+        <img src="${dato.image}" class="col-md-12 rounded-5" alt="${dato.name}"
+        style="height:100%; width: 100%;">
+    </div>
+<div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${dato.name}</h5>
+            <p class="card-text">${dato.category}</p>
+            <div class="row">
+                <p class="card-text col-md-6"><small class="text-muted ">price: $ ${dato.price}</small></p>
+                <a href="#" class=" btn btn-dark col-md-6 rounded-5 align-self-end bg-black">Read more</a>
             </div>
         </div>
-        `
-    }
-}
-contenido(contenedor)
+    </div>
+</div>
+</div> 
+`
+));
+
+
+let boton = document.getElementById("buttonjS");
+boton.addEventListener("click",function(click){
+    console.log(click);
+})
+
+
+let buscador = document.getElementById("searchJS");
+buscador.addEventListener("keyup",function(eve){
+    console.log(eve.target.value);
+})
