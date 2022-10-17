804e41488080d68fd18ea99ffb168739fbb0a794
zincel <106347425+zincel@users.noreply.github.com>
2022-10-17 09:38:56 -0300

    filtro de search

--- js/index.js
+++ js/index.js
@@ -27,12 +27,46 @@ let tarjet = datos.map(dato => (contenedorTarjet.innerHTML +=
 
 
 let boton = document.getElementById("buttonjS");
-boton.addEventListener("click",function(click){
-    console.log(click);
+boton.addEventListener("click",function(event){
+    console.log(event);
 })
 
 
 let buscador = document.getElementById("searchJS");
-buscador.addEventListener("keyup",function(eve){
-    console.log(eve.target.value);
+
+buscador.addEventListener("keyup", function (event){
+    let nombres = datos.filter((value) => value.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    contenedorTarjet.innerHTML = ""
+    nombres.forEach(resultado)
+    console.log(nombres);
 })
+
+
+
+
+
+function resultado(params) {
+    contenedorTarjet.innerHTML +=
+    `
+<div class=" colorDetexto card mb-3 p-0 rounded-5  bg-dark" style="max-width: 540px;">
+<div class="row g-0" style="height: 100%;">
+    <div class="col-md-4 ">
+        <img src="${params.image}" class="col-md-12 rounded-5" alt="${params.name}"
+        style="height:100%; width: 100%;">
+    </div>
+<div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${params.name}</h5>
+            <p class="card-text">${params.category}</p>
+            <div class="row">
+                <p class="card-text col-md-6"><small class="text-muted ">price: $ ${params.price}</small></p>
+                <a href="#" class=" btn btn-dark col-md-6 rounded-5 align-self-end bg-black">Read more</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div> 
+`
+}
+
+
