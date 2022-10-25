4aa0d086c8f1db1026bcd1e4bed28a5613e495e7
zincel <106347425+zincel@users.noreply.github.com>
2022-10-25 04:59:12 -0300

    aplique el uso de una api

--- js/past-events.js
+++ js/past-events.js
@@ -4,16 +4,13 @@ let $checkobox = document.getElementById("checkboxJS");
 
 let $buscador = document.getElementById("searchJS");
 
-
-
-
 let pastEvents;
-fetch('https://amazing-events.herokuapp.com/api/events')
+
+fetch('https://mh-amazing.herokuapp.com/amazing')
     .then(data => data.json())
     .then(res => {
         eventos = res.events;
-        pastEvents = eventos.filter(event => res.currentDate > event.date)
-        console.log(pastEvents);
+        pastEvents = eventos.filter(event => res.date > event.date)
         crearCheckbox(pastEvents, $checkobox)
         imprimirCards(pastEvents, $cards)
         $buscador.addEventListener('keyup', filtrar)
@@ -21,11 +18,11 @@ fetch('https://amazing-events.herokuapp.com/api/events')
     })
 
 
-
+//funcion para crear checkbox
 
 function crearCheckbox(eventos, contenedor) {
     let fn = eventos => eventos.category
-    let categorias = new Set(eventos.filter(fn).map(fn))
+    let categorias = new Set(eventos.map(fn))
     categorias.forEach(even => {
         contenedor.innerHTML +=
             `
@@ -46,7 +43,6 @@ function crearCard(array) {
     section.innerHTML +=
 
         `
-        
         <div class="row g-0" style="height: 100%;">
             <div class="col-md-4 ">
                 <img src="${array.image}" class="col-md-12 rounded-5" alt="${array.name}"
@@ -58,7 +54,7 @@ function crearCard(array) {
                     <p class="card-text">${array.category}</p>
                     <div class="row">
                         <p class="card-text col-md-6"><small class="text-muted ">price: $ ${array.price}</small></p>
-                        <a href="./details.html?id=${array._id}" class=" btn btn-dark col-md-6 rounded-5 align-self-end bg-black">Read more</a>
+                        <a href="./details.html?id=${array.id}" class=" btn btn-dark col-md-6 rounded-5 align-self-end bg-black">Read more</a>
                     </div>
                 </div>
             </div>
@@ -70,22 +66,21 @@ function crearCard(array) {
 
 // funcion para imprimir cards
 
-function imprimirCards(pastEvents, contenedor) {
+function imprimirCards(eventos, contenedor) {
     contenedor.innerHTML = ''
-    if (pastEvents.length > 0) {
+    if (eventos.length > 0) {
         let fragment = document.createDocumentFragment()
-        pastEvents.forEach(pastEvents => fragment.appendChild(crearCard(pastEvents, contenedor)))
+        eventos.forEach(eventos => fragment.appendChild(crearCard(eventos, contenedor)))
         contenedor.appendChild(fragment)
-
     } else {
         contenedor.innerHTML = '<h4>error</h4>'
     }
 }
 
-
+// funcion para filtrar
 function filtrar() {
     let checked = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(ele => ele.value)
-    let filtradosPor = pastEvents.filter(pastEvents => checked.includes(pastEvents.category) || checked.length === 0)
-    let filtradosPorSearch = filtradosPor.filter(pastEvents => pastEvents.name.toLowerCase().includes($buscador.value.toLowerCase()))
+    let filtradosPor = pastEvents.filter(eventos => checked.includes(eventos.category) || checked.length === 0)
+    let filtradosPorSearch = filtradosPor.filter(eventos => eventos.name.toLowerCase().includes($buscador.value.toLowerCase()))
     imprimirCards(filtradosPorSearch, $cards)
 }
